cb2c366d40ecd0bdd3203ea28e50108b8a5dd39c
Vtres <viniciust98@outlook.com>
2021-03-06 11:13:29 -0300

    enviando widget

---
+++ widget/academy-occ-d01-viniciusTres/js/academy-occ-d01-viniciusTres.js
@@ -0,0 +1,23 @@
+define(
+    // Dependencies
+    ['jquery', 'knockout'],
+
+    // Module Implementation
+    function($,ko) {
+      
+      "use strict"
+
+
+      return {
+        // variavel:ko.observable()
+        onLoad:function(widget){
+          //chama funcoes 
+        },
+
+        beforeAppear:function(){
+
+        }
+
+      };
+    }
+);
